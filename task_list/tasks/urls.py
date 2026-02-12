423933eb825bc13ff8864e99060d770a36c11d23
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-02-12 13:11:54 +0000

    Add urls, views and home page

--- task_list/tasks/urls.py
+++ task_list/tasks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name="home"),
+    path('edit/<task_id>/', views.edit, name="edit"),
+    path('delete/<task_id>/', views.delete, name="delete"),
 ]
--- task_list/tasks/views.py
+++ task_list/tasks/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TaskList
+from .forms import AddTask
 
-# Create your views here.
+def home(request):
+    tasks = TaskList.objects.all()
+    form = AddTask()
+    if request.method == 'POST':
+        form = AddTask(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')        
+    context = {'form':form, 'tasks':tasks}
+    return render(request, 'home.html', context)
+
+def edit(request, task_id):
+    task = TaskList.objects.get(id=task_id)
+    task.done = True
+    task.save()
+    return redirect('home')
+
+def delete(request, task_id):
+    task = TaskList.objects.get(id=task_id)
+    task.delete()
+    return redirect('home')

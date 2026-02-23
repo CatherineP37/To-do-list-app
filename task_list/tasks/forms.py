3dad57c8654f84a6fa361d6302fb663f4db543fb
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-02-23 07:35:28 +0000

    Add placeholder text to form input box

--- task_list/tasks/forms.py
+++ task_list/tasks/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import TaskList
 
@@ -5,3 +6,7 @@ class AddTask(ModelForm):
     class Meta:
         model = TaskList
         fields = ['title']
+
+        widgets = {
+            'title': forms.textInput(attrs={'placeholder':'Enter your task here'}),
+        }
